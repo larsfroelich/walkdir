68ff528086d307c14ddb9926ab5323a06926c03a
Lars Frölich <lars.froelich@gmail.com>
2017-11-30 17:07:22 +0100

    added listSync, list, documentation, as well as Promises and Callbacks

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# WalkDir
+# walkDir
 NPM-Module used to "walk" through a directory, revealing the contained folders and files in a practical way.
+
+## Why
+Oftentimes when working with a node.js application, it is necessary to retrieve the structure of a certain folder in an easy-to-parse form.
+This is useful e.g. when building an express.js-router or for bootstrapping child-modules automatically.
+
+##Setup
+Install walkDir via `npm install walkdir --save`
+
+## Usage
+1) Require walkDir using
+    
+    `var walkDir = require("walkdir");`
+2) Choose one of the following methods it provides:
+
+    2.1 **List**
+    
+        `walkDir.list(excludedPath, includedPath, depth)`
+        
+    + `excludedPath`: beginning of your search-path which should **not** be included in the results.
+    
+    + `includedPath`: end of your search-path which **should** be included in the results.
+    
+    + `depth`: At what level of depth should the recursive search end?
+        
+        + 0: Never. End when all subdirectories have been found and listed.
+        + x: End after peeking into x levels of folders, 1 being the provided path
+
+    + **returns**: Array of Strings representing the paths to found folders and files
+    
+3) Decide on using the *synchronous*, *promise*-based or *callback*-based version
+    
+    3.1 **Synchronous**
+    
+    + All methods are available as Synchronous versions by simply appending "Sync" to it
+    + e.g. list**Sync**, tree**Sync**
+    
+    3.2 **Promises**
+    
+    + All methods return [*promises*]("https://developers.google.com/web/fundamentals/primers/promises")
+    + simply use them to react to success/failure of the given method
+    + e.g. `list(...).then(function(result){/** do sth with result **/ }).catch(function(err){/** react to error **/)`
+    
+    3.3 **Callbacks**
+    
+    + All methods accept a *callback*-function as optional last argument
+    + simply pass this function using the signature `function(err, result){...}`
+    + e.g. `list(..., function(err, result){/** do sth with result if !err */})`
--- index.js
+++ index.js
@@ -1,5 +1,60 @@
+const fs = require('fs-extra');
+function errorResponse(resolve, reject, callback, error){
+    resolve = null;
+    reject(error);
+    if(callback && typeof(callback) === 'function'){
+        callback(error, null);
+    }
+}
+function successResponse(resolve, callback, result){
+    if(resolve !== null){
+        resolve(result);
+        if(callback && typeof(callback) === 'function'){
+            callback(null, result);
+        }
+    }
+}
 module.exports = {
-    list: function(excludedPath, includedPath, depth){
-
+    listSync: function listSync(excludedPath, includedPath, depth){
+        var results = [];
+        var list = fs.readdirSync(excludedPath + includedPath);
+        list.forEach(function(file) {
+            var stat = fs.statSync(excludedPath + includedPath + '/' + file);
+            if (stat && stat.isDirectory() && (depth > 1 || depth === -1))
+                results = results.concat(listSync(excludedPath, includedPath + '/' + file, (depth > 1 ? depth-1 : depth)));
+            else if (stat && !stat.isDirectory()) results.push(includedPath + '/' + file)
+        });
+        return results;
+    },
+    list: function list(excludedPath, includedPath, depth, callback){
+        return new Promise(function(resolve, reject){
+            var paths = [];
+            var recursivePromises = [];
+            fs.readdir(excludedPath + includedPath, function(err, folderItems){
+                if(err){
+                    errorResponse(resolve, reject, callback, err);
+                }else{
+                    folderItems.forEach(function(file) {
+                        fs.stat(excludedPath + includedPath + '/' + file, function(err, stat){
+                            if(err){
+                                recursivePromises.push(new Promises(function(resolve, reject){reject(err);}));
+                            }else{
+                                if (stat && stat.isDirectory() && (depth > 1 || depth === 0)){ // is a directory and depth ok?
+                                    recursivePromises.push(list(excludedPath, includedPath + '/' + file, (depth > 1 ? depth-1 : depth)));
+                                }else if (stat && !stat.isDirectory()){
+                                    paths.push(includedPath + '/' + file)
+                                }
+                            }
+                        });
+                    });
+                    Promise.all(recursivePromises).then(function(results){
+                        paths = paths.concat(results);
+                    }).catch(function(err){
+                        errorResponse(resolve, reject, callback, err);
+                    });
+                    successResponse(resolve, callback, paths);
+                }
+            });
+        })
     }
 };
